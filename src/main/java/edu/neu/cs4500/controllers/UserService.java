04ea5733ba57cb9e7a62124ddadb46558b022963
Benjamin Reed <reed.b@husky.neu.edu>
2019-01-23 20:37:03 -0500

    User Service First Function

---
+++ src/main/java/edu/neu/cs4500/controllers/UserService.java
@@ -0,0 +1,21 @@
+package edu.neu.cs4500.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.ArrayList;
+
+@RestController
+public class UserService {
+    static List<User> users = new ArrayList();
+    static  {
+        users.add(new User(123, "alice", "alice", "Alive", "Wonderland"));
+        users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+    }
+    @GetMapping("/api/user")
+    public List<User>findAllUsers() {
+        return users;
+    }
+}
